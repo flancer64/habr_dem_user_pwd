3c50892a2d3f69b8760848bc8a6e945b1a078207
Alex Gusev <alex@flancer64.com>
2021-08-31 11:26:12 +0300

    WF-490 Habr: DEM impl

---
+++ src/Back/Api/Defaults.mjs
@@ -0,0 +1,11 @@
+/**
+ * Plugin constants (hardcoded configuration) for backend code.
+ */
+export default class Fl64_Dem_User_Pwd_Back_Api_Defaults {
+
+    NAME = '@flancer64/habr_dem_user_pwd';
+
+    constructor() {
+        Object.freeze(this);
+    }
+}
